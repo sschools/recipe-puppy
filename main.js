067f7137480a76edeb63fb0e5d70dbc794539b4b
Stephen Schools <stephen.schools>
2017-08-08 15:06:29 -0400

    data loading and on page... working on layout and placeholder picture

---
+++ main.js
@@ -0,0 +1,29 @@
+let myData;
+let text;
+let target = document.querySelector("#container");
+let submit = document.querySelector("#submit");
+
+function getFood() {
+  text = document.getElementById("input").value;
+  console.log(text);
+  return fetch("https://proxy.calweb.xyz/http://www.recipepuppy.com/api/?q=" + text)
+  .then (function(response){
+    return response.json();
+  }).then (function(data) {
+    buildHtml(data);
+  });
+}
+
+function buildHtml(recipes) {
+  let html = "";
+  for (let recipe of recipes.results) {
+    html += `<figure><img src="${recipe.thumbnail}"/>
+      <figcaption><a href="${recipe.href}">${recipe.title}</a></figcaption>
+      </figure>
+      `;
+    console.log(recipe.title);
+  }
+  target.innerHTML = html;
+}
+
+submit.addEventListener("click", getFood);
